e5ac639c1fa2afbb4fab8ebf442b79cf4c9d9f54
Grayton Ward <graytonio.ward@gmail.com>
2024-04-27 20:30:10 -0400

    fix: add helm dependency build

--- manifests/generate-helm.sh
+++ manifests/generate-helm.sh
@@ -7,14 +7,7 @@ export FLAGOPS_IDENTITY=${ARGOCD_APP_NAME}
 export FLAGOPS_DESTINATION_TYPE="file"
 export FLAGOPS_DESTINATION_PATH=$(mktemp -d)
 
->&2 echo "DEBUG PWD: $(pwd)"
->&2 echo "DEBUG LS: $(ls)"
-
 # Run Generation
->&2 echo "INFO Running FlagOps"
 flagops --use-env
 
->&2 echo "DEBUG LS: $(ls ${FLAGOPS_DESTINATION_PATH})"
-
->&2 echo "INFO Inflating rendered templates"
-helm template $ARGOCD_APP_NAME -n $ARGOCD_APP_NAMESPACE ${FLAGOPS_DESTINATION_PATH}
+helm dependency build && helm template $ARGOCD_APP_NAME -n $ARGOCD_APP_NAMESPACE ${FLAGOPS_DESTINATION_PATH}
